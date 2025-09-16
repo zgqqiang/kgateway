73ea4bb057937b4d3ab3e4a7487bdc7668ce31db
Shashank Ram <21697719+shashankram@users.noreply.github.com>
2025-09-16 09:41:10 -0700

    api/rate-limit: reject invalid fillInterval value (#12286)
    Signed-off-by: Shashank Ram <shashank.ram@solo.io>

--- api/v1alpha1/traffic_policy_types.go
+++ api/v1alpha1/traffic_policy_types.go
@@ -308,6 +308,7 @@ type TokenBucket struct {
 	// It determines the frequency of token replenishment.
 	// +required
 	// +kubebuilder:validation:XValidation:rule="matches(self, '^([0-9]{1,5}(h|m|s|ms)){1,4}$')",message="invalid duration value"
+	// +kubebuilder:validation:XValidation:rule="duration(self) >= duration('50ms')",message="must be at least 50ms"
 	FillInterval metav1.Duration `json:"fillInterval"`
 }
 
